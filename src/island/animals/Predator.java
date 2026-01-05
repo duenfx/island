2f7b745d2244941886ddbaf2b4fa13e7657ee9e0
karin <karinahanzha@gmail.com>
2026-01-05 13:55:05 +0200

    Overrided abstract methods from Animal.java in Predator.java

--- src/island/animals/Predator.java
+++ src/island/animals/Predator.java
@@ -1,4 +1,24 @@
 package island.animals;
 
-public class Predator {
+import island.Cell;
+
+public class Predator extends Animal {
+    public Predator(String name, int weight, int speed) {
+        super(name, weight, speed);
+    }
+
+    @Override
+    public void eat(Cell cell) {
+        System.out.println(name + " їсть інших тварин.");
+    }
+
+    @Override
+    public void reproduce(Cell cell) {
+        System.out.println(name + " розмножується.");
+    }
+
+    @Override
+    public void move(Cell cell) {
+        System.out.println(name + " пересувається");
+    }
 }
