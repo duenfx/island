0ac532e919d1eab3880df81386f71392f68657ac
karin <karinahanzha@gmail.com>
2026-01-05 17:12:44 +0200

    Add basic logic in Island.java

--- src/island/Island.java
+++ src/island/Island.java
@@ -1,4 +1,30 @@
 package island;
 
 public class Island {
+    private int width;
+    private int height;
+    private Cell[][] cells;
+
+    public Island(int width, int height) {
+        this.width = width;
+        this.height = height;
+        cells = new Cell[height][width];
+        for(int a = 0; a < height; a++) {
+            for(int b = 0; b < width; b++) {
+                cells[a][b] = new Cell();
+            }
+        }
+    }
+
+    public Cell getCell(int a, int b) {
+        return cells[a][b];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
