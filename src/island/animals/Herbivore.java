a78f7c22849765c93c03fdcfe7b1b1eecdb67245
karin <karinahanzha@gmail.com>
2026-01-05 13:50:52 +0200

    Overrided abstract methods from Animal.java in Herbivore.java

--- src/island/animals/Herbivore.java
+++ src/island/animals/Herbivore.java
@@ -1,4 +1,24 @@
 package island.animals;
 
-public class Herbivore {
+import island.Cell;
+
+public class Herbivore extends Animal {
+    public Herbivore(String name, int weight, int speed) {
+        super(name, weight, speed);
+    }
+
+    @Override
+    public void eat(Cell cell) {
+        System.out.println(name + " їсть рослини.");
+    }
+
+    @Override
+    public void reproduce(Cell cell) {
+        System.out.println(name + " розмножується.");
+    }
+
+    @Override
+    public void move(Cell cell) {
+        System.out.println(name + " пересувається");
+    }
 }
