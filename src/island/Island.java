5de218405056d41c29f22923fa0cde4c41ce204f
karin <karinahanzha@gmail.com>
2026-01-05 19:25:34 +0200

    Add initialization for all animal types

--- src/island/Island.java
+++ src/island/Island.java
@@ -1,5 +1,11 @@
 package island;
 
+import island.animals.predators.*;
+import island.animals.herbivores.*;
+import island.plants.Plant;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Island {
     private int width;
     private int height;
@@ -12,6 +18,94 @@ public class Island {
         for(int a = 0; a < height; a++) {
             for(int b = 0; b < width; b++) {
                 cells[a][b] = new Cell();
+            }
+        }
+    }
+
+    public void init() {
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                Cell cell = cells[i][j];
+
+                int wolfCount = ThreadLocalRandom.current().nextInt(0,31);
+                for(int k = 0; k < wolfCount; k++) {
+                    cell.addAnimal((new Wolf()));
+                }
+
+                int boaCount = ThreadLocalRandom.current().nextInt(0,31);
+                for(int k = 0; k < boaCount; k++) {
+                    cell.addAnimal((new Boa()));
+                }
+
+                int foxCount = ThreadLocalRandom.current().nextInt(0,31);
+                for(int k = 0; k < foxCount; k++) {
+                    cell.addAnimal((new Fox()));
+                }
+
+                int bearCount = ThreadLocalRandom.current().nextInt(0,6);
+                for(int k = 0; k < bearCount; k++) {
+                    cell.addAnimal((new Bear()));
+                }
+
+                int eagleCount = ThreadLocalRandom.current().nextInt(0,21);
+                for(int k = 0; k < eagleCount; k++) {
+                    cell.addAnimal((new Eagle()));
+                }
+
+                int horseCount = ThreadLocalRandom.current().nextInt(0,21);
+                for(int k = 0; k < horseCount; k++) {
+                    cell.addAnimal((new Horse()));
+                }
+
+                int deerCount = ThreadLocalRandom.current().nextInt(0,21);
+                for(int k = 0; k < deerCount; k++) {
+                    cell.addAnimal((new Deer()));
+                }
+
+                int rabbitCount = ThreadLocalRandom.current().nextInt(0,151);
+                for(int k = 0; k < rabbitCount; k++) {
+                    cell.addAnimal((new Rabbit()));
+                }
+
+                int mouseCount = ThreadLocalRandom.current().nextInt(0,501);
+                for(int k = 0; k < mouseCount; k++) {
+                    cell.addAnimal((new Mouse()));
+                }
+
+                int goatCount = ThreadLocalRandom.current().nextInt(0,141);
+                for(int k = 0; k < goatCount; k++) {
+                    cell.addAnimal((new Goat()));
+                }
+
+                int sheepCount = ThreadLocalRandom.current().nextInt(0,141);
+                for(int k = 0; k < sheepCount; k++) {
+                    cell.addAnimal((new Sheep()));
+                }
+
+                int boarCount = ThreadLocalRandom.current().nextInt(0,51);
+                for(int k = 0; k < boarCount; k++) {
+                    cell.addAnimal((new Boar()));
+                }
+
+                int buffaloCount = ThreadLocalRandom.current().nextInt(0,11);
+                for(int k = 0; k < buffaloCount; k++) {
+                    cell.addAnimal((new Buffalo()));
+                }
+
+                int duckCount = ThreadLocalRandom.current().nextInt(0,201);
+                for(int k = 0; k < duckCount; k++) {
+                    cell.addAnimal((new Duck()));
+                }
+
+                int caterpillarCount = ThreadLocalRandom.current().nextInt(0,1001);
+                for(int k = 0; k < caterpillarCount; k++) {
+                    cell.addAnimal((new Caterpillar()));
+                }
+
+                int plantCount = ThreadLocalRandom.current().nextInt(0,201);
+                for(int k = 0; k < plantCount; k++) {
+                    cell.addPlant((new Plant()));
+                }
             }
         }
     }
