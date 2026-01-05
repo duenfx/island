1dcdc5c3b2a35dc542e1352ec50489a090b27d35
karin <karinahanzha@gmail.com>
2026-01-05 19:02:20 +0200

    Renamed Caterpillar.java

---
+++ src/island/animals/herbivores/Caterpillar.java
@@ -0,0 +1,9 @@
+package island.animals.herbivores;
+
+import island.animals.Herbivore;
+
+public class Caterpillar extends Herbivore {
+    public Caterpillar() {
+        super("Caterpillar", 0.01, 0, 0, 0);
+    }
+}
--- src/island/animals/herbivores/Cetarpillar.java
+++
@@ -1,9 +0,0 @@
-package island.animals.herbivores;
-
-import island.animals.Herbivore;
-
-public class Cetarpillar extends Herbivore {
-    public Cetarpillar() {
-        super("Cetarpillar", 0.01, 0, 0, 0);
-    }
-}
