60adadd1c8cb5ebf0c77c5ab67c99e7274d968ff
karin <karinahanzha@gmail.com>
2026-01-05 13:33:35 +0200

    Add logic to Cell.java

--- src/island/Cell.java
+++ src/island/Cell.java
@@ -1,4 +1,33 @@
 package island;
 
+import island.animals.Animal;
+import island.plants.Plant;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
+    private List<Animal> animals;
+    private List<Plant> plants;
+
+    public Cell() {
+        animals = new ArrayList<>();
+        plants = new ArrayList<>();
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
 }
