25ff7fdc95976409e3aab0944d5a40a03da7aaae
karin <karinahanzha@gmail.com>
2026-01-05 13:39:53 +0200

    Add abstract class Animal.java and basic logic

--- src/island/animals/Animal.java
+++ src/island/animals/Animal.java
@@ -1,4 +1,22 @@
 package island.animals;
 
-public class Animal {
+import island.Cell;
+
+public abstract class Animal {
+    protected String name;
+    protected int weight;
+    protected int speed;
+
+    public Animal(String name, int weight, int speed) {
+        this.name = name;
+        this.weight = weight;
+        this.speed = speed;
+    }
+
+    public abstract void eat(Cell cell);
+
+    public abstract void reproduce(Cell cell);
+
+    public abstract void move(Cell cell);
+
 }
