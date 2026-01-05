51b67048c1b84f1ae30f9d20cedfa78c01db28b6
karin <karinahanzha@gmail.com>
2026-01-05 20:38:44 +0200

    Update eat method in Predator.java

--- src/island/animals/Predator.java
+++ src/island/animals/Predator.java
@@ -2,6 +2,9 @@ package island.animals;
 
 import island.Cell;
 
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Predator extends Animal {
     public Predator(String name, double weight, int speed, double foodNeeded, double currentSatiety) {
         super(name, weight, speed, foodNeeded, currentSatiety);
@@ -9,7 +12,33 @@ public class Predator extends Animal {
 
     @Override
     public void eat(Cell cell) {
-        System.out.println(name + " їсть інших тварин.");
+        var animals = cell.getAnimals();
+        Iterator<Animal> iterator = animals.iterator();
+
+        while (iterator.hasNext()) {
+            Animal victim = iterator.next();
+            if (this == victim || this.getClass() == victim.getClass()) {
+                continue;
+            }
+            int chance = FoodChain.getProbability(this, victim);
+            if (chance > 0) {
+                int dice = ThreadLocalRandom.current().nextInt(100);
+                if (dice < chance) {
+                    double foodGained = victim.getWeight();
+                    if (this.currentSatiety + foodGained > this.foodNeeded) {
+                        this.currentSatiety = this.foodNeeded;
+                    } else {
+                        this.currentSatiety += foodGained;
+                    }
+
+                    System.out.println(this.name + " з'їв " + victim.getClass().getSimpleName());
+                    victim.die();
+                    iterator.remove();
+                    return;
+                }
+            }
+        }
+        System.out.println(this.name + " нікого не спіймав.");
     }
 
     @Override
