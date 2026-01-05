174000f82820c51f7f2fe71a1ae52bf41bd50783
karin <karinahanzha@gmail.com>
2026-01-05 18:29:45 +0200

    Add Wolf.java and Rabbit.java with parameters

---
+++ src/island/animals/herbivores/Rabbit.java
@@ -0,0 +1,9 @@
+package island.animals.herbivores;
+
+import island.animals.Herbivore;
+
+public class Rabbit extends Herbivore {
+    public Rabbit() {
+        super("Rabbit", 2, 2, 0.45, 0);
+    }
+}
---
+++ src/island/animals/predators/Wolf.java
@@ -0,0 +1,9 @@
+package island.animals.predators;
+
+import island.animals.Predator;
+
+public class Wolf extends Predator {
+    public Wolf() {
+        super("Wolf", 50, 3, 8, 0);
+    }
+}
